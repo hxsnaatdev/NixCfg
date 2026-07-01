86b1fff7c2bfcb407b8ea0e45dabfbae1cf173be
hxsnaatdev <arizzzzzler@gmail.com>
2026-07-01 18:16:53 +0530

    feat(kanata/tray): hardcoded path to run kanata
    req services and tray module hardcoded and supressed when error occur
    keeping in very near to hte cpu

---
+++ hosts/M4/kanata-launchd.nix
@@ -0,0 +1,25 @@
+{...}: {
+  launcd.daemons.Kanata-local = {
+    scipt = "sudo --preserve-env /Users/ariz/.config/keyboard/kanata/executables/macos-binaries-arm64/kanata_macos_cmd_allowed_arm64 --nodelay --cfg /Users/ariz/.config/keyboard/kanata/ariz.kbd";
+    serviceConfig = {
+      RunAtLoad = true;
+      keepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+      Nice = -19;
+    };
+  };
+
+  launchd.user.agents.Kanata-tray-local = {
+    command = "sudo --preserve-env /Users/ariz/.config/keyboard/kanata/executables/kanata-tray/kanata-tray-macos";
+    serviceConfig = {
+      RunAtLoad = true;
+      keepAlive = {
+        Crashed = true;
+        SuccessfulExit = false;
+      };
+      Nice = -19;
+    };
+  };
+}

ee0b7f0353c5deb8306becac3754e3528c861a44
hxsnaatdev <arizzzzzler@gmail.com>
2026-07-13 22:47:23 +0530

    chore(refact/import): disamiguate module and import hm declarative browsing

---
+++ home/home.nix
@@ -0,0 +1,23 @@
+{lib, ...}: {
+  imports = [
+    ./dev/git.nix
+    ./tty/shells/fish.nix
+    ./tty/starship.nix
+    ./syncthing.nix
+    ./web/chromium.nix
+    ./web/firefox.nix
+  ];
+  home.username = "ariz";
+  home.homeDirectory = lib.mkForce "/Users/ariz";
+  home.stateVersion = "26.05";
+  home.enableNixpkgsReleaseCheck = false;
+  home.file.".hushlogin".text = "";
+
+  programs.home-manager.enable = true;
+  programs.warpd.enable = true;
+  programs.warpd.package = null;
+  programs.atuin = {
+    enable = true;
+    enableFishIntegration = true;
+  };
+}
